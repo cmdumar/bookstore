eb3ce81f1479c7ce16013c34c75ec8997f0c9999
unknown <mohammadumar28@gmail.com>
2021-01-27 08:16:14 +0530

    Style the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
+import Navbar from './Navbar';
 import BooksList from './BooksList';
 import BooksForm from './BooksForm';
 
 const App = () => (
-  <div>
+  <>
+    <Navbar />
     <BooksList />
     <BooksForm />
-  </div>
+  </>
 );
 
 export default App;
